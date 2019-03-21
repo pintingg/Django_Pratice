a07f1c49cd03ef9868f2be0a9875c7287482a7b0
PinTing <jk851023@gmail.com>
2019-03-21 15:52:18 +0800

    Update code
    make api web page 404 not found.

--- practice/static/practice/mine.js
+++ practice/static/practice/mine.js
@@ -2,21 +2,33 @@ myfunction();
 function myfunction(){
 	console.log("I am in the function now!");
 	var csrftoken = Cookies.get("csrftoken");
-	/*$.ajax({
+	function csrfSafeMethod(method) {
+	    // these HTTP methods do not require CSRF protection
+	    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+	}
+	$.ajaxSetup({
+	    beforeSend: function(xhr, settings) {
+	        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+	            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+	        }
+	    }
+	});
+	$.ajax({
 	    type: 'POST',
 	    url: "/post",
 	    data: {
-	        "partner_ref": "PH",
-	        "return_field": ["summary", "details"]
+	        "a": "0",
+	        "b": "2"
 	    },
-	    success: function(data){
-
+	    success: function(response){
+	    	console.log("Received");
+	    	console.log(response.data);
 	    },
 	    dataType: 'json'
-	});*/
-	$.ajaxSetup({
-    	data: {csrfmiddlewaretoken: csrftoken},
 	});
+	/*$.ajaxSetup({
+    	data: {csrfmiddlewaretoken: csrftoken},
+	}); 
 	$.post({
 	    url : "/post",
 	    data : {a : "test"},
@@ -25,5 +37,5 @@ function myfunction(){
 	    	console.log(response.data);
 	    },
 	    dataType : "json"
-	});
+	});*/
 };
--- practice/views.py
+++ practice/views.py
@@ -1,13 +1,19 @@
 from django.contrib.staticfiles.templatetags import staticfiles
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+
+
 # Create your views here.
 
 def test(request):
 	return render(request, 'practice/test.html')
 
-
+@csrf_exempt
 def post(request):
-	print(request.method)
-	print(request.POST.get('a', 'Not get'))
-	return JsonResponse({'data' : 'Succeed'})
+	data = request.POST
+	if data:
+		print('{} is {}.'.format(data['a'], data['b']))
+		return JsonResponse({'data' : 'Succeed'})
+	else:
+		return HttpResponseNotFound()
